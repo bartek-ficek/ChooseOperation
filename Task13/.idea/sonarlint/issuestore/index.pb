91d6ecb052b424f2b0f6092cbb76d10fa7d7e03b
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-08-27 22:01:42 +0200

    SwitchInstructionDone

---
+++ Task13/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+E
+src/LearnBF/Main.java,6/c/6cc6ed3a7fecd63bd46e20806e9b8582ff620eaa
--- Task13/src/LearnBF/Main.java
+++ Task13/src/LearnBF/Main.java
@@ -1,8 +1,61 @@
 package LearnBF;
 
+import jdk.dynalink.Operation;
+
+import java.util.Scanner;
+
 public class Main {
+    public enum Operations {
+        NEW_SENTENCE,
+        SUBSTRING,
+        CONCATENATION,
+        REPLACE,
+        END_PROGRAM
+    }
+    Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-	// write your code here
+        Main Scenario = new Main();
+        Scenario.run();
     }
+
+    public void run() {
+        System.out.println("Hi! Choose what you want to do!");
+        for (int i = 0; i < Operations.values().length; i++) {
+            System.out.println(i + ". " + Operations.values()[i]);
+        }
+        System.out.print("Type the number of the operation: ");
+        Integer yourChoice = scanner.nextInt();
+        System.out.println();
+
+
+        Operations choice = Operations.values()[yourChoice];
+        switch (choice) {
+            case CONCATENATION: {
+                funConcatenation();
+                break;
+            }
+            case SUBSTRING: {
+                funSubstring();
+                break;
+            }
+            case NEW_SENTENCE: {
+                funNewSentence();
+                break;
+            }
+            case REPLACE: {
+                funReplace();
+                break;
+            }
+            case END_PROGRAM: {
+                funEndProgram();
+                break;
+
+            }
+
+        }
+    }
+
 }
+
